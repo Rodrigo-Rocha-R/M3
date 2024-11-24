250315519e272ca44d86121b9e70c3b08b8dcebf
Rodrigo_R <110933152+Rodrigo-Rocha-R@users.noreply.github.com>
2024-11-24 14:26:52 -0600

    Files

---
+++ trafficLights.py
@@ -0,0 +1,24 @@
+traffic_light_coords = [
+    
+
+    [(6, 21), (7, 21), 1],  # Semaphore 1 - verde
+    [(8, 22), (8, 23), 1],  # Pair of Semaphore 1 - rojo
+
+    [(6, 16), (7, 16), 1],  # Semaphore 2 - verde
+    [(8, 18), (8, 17), 1],  # Pair of Semaphore 2 - rojo
+
+    [(0, 6), (1, 6), 1],  # Semaphore 3 - verde
+    [(2, 4), (2, 5), 1],  # Pair of Semaphore 3 - rojo
+
+    [(5, 0), (5, 1), 1],  # Semaphore 4 - rojo
+    [(6, 2), (7, 2), 1],  # Pair of Semaphore 4 - verde
+
+    [(17, 8), (17, 9), 1],  # Semaphore 5 - rojo
+    [(18, 7), (19, 7), 1],  # Pair of Semaphore 5 - verde
+    
+    
+    
+]
+
+
+
