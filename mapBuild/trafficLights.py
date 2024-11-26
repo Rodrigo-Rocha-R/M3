4ac39f0503a5c3b882332f8cfda3c389ff951533
nicoleKape-code <110926449+nicoleKapellmann@users.noreply.github.com>
2024-11-26 10:36:42 -0600

    ¨trafficLights¨

--- mapBuild/trafficLights.py
+++ mapBuild/trafficLights.py
@@ -1,42 +1,19 @@
 traffic_light_coords = [
 
+    [(6, 21), (7, 21), "red"],  # Semaphore 1 - verde
+    [(8, 22), (8, 23), "green"],  # Pair of Semaphore 1 - rojo
 
-    [(6, 21), "yellow"],
-    [(7, 21), "yellow"],  # Semaphore "yellow" - verde
-    [(8, 22), "yellow"],  
-    [(8, 23), "yellow"],  # Pair of Semaphore "yellow" - rojo
+    [(6, 16), (7, 16), "red"],  # Semaphore 2 - verde
+    [(8, 18), (8, 17), "green"],  # Pair of Semaphore 2 - rojo
 
-    [(6, 16), "yellow"],  # Semaphore 2 - verde
-    [ (7, 16), "yellow"],  # Semaphore 2 - verde
+    [(0, 6), (1, 6), "red"],  # Semaphore 3 - verde
+    [(2, 4), (2, 5), "green"],  # Pair of Semaphore 3 - rojo
 
-    [(8, 18), "yellow"],  # Pair of Semaphore 2 - rojo
-    [(8, 17), "yellow"],  # Pair of Semaphore 2 - rojo
+    [(5, 0), (5, 1), "red"],  # Semaphore 4 - rojo
+    [(6, 2), (7, 2), "green"],  # Pair of Semaphore 4 - verde
 
-
-    [(0, 6), "yellow"],  # Semaphore 3 - verde
-
-    [(1, 6), "yellow"],  # Semaphore 3 - verde
-
-    [(2, 4),  "yellow"],  # Pair of Semaphore 3 - rojo
-    [ (2, 5), "yellow"],  # Pair of Semaphore 3 - rojo
-
-    [(5, 0), "yellow"],  # Semaphore 4 - rojo
-
-    [(5, 1), "yellow"],  # Semaphore 4 - rojo
-
-
-    [(6, 2), "yellow"],  # Pair of Semaphore 4 - verde
-
-    [ (7, 2), "yellow"],  # Pair of Semaphore 4 - verde
-
-
-    [(17, 8), "yellow"],  # Semaphore 5 - rojo
-
-    [(17, 9), "yellow"],  # Semaphore 5 - rojo
-
-    [(18, 7), "yellow"],  # Pair of Semaphore 5 - verde
-
-    [ (19, 7), "yellow"],  # Pair of Semaphore 5 - verde
+    [(17, 8), (17, 9), "red"],  # Semaphore 5 - rojo
+    [(18, 7), (19, 7), "green"],  # Pair of Semaphore 5 - verde
     
     
 ]
